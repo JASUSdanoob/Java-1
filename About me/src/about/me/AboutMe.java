220b372218ef0428582b0674e977955781260405
Jesus Resendiz <19jresendizestrella@nuview.k12.ca.us>
2017-07-10 09:46:56 -0700

    about me

---
+++ About me/src/about/me/AboutMe.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package about.me;
+
+/**
+ *
+ * @author Education Unlimited
+ */
+public class AboutMe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("My name is Jesus Resendiz");
+        System.out.println("I am 15");
+        System.out.println("My hometown is Perris");
+        System.out.println("I like to play cards, Basketball, and video games");
+        System.out.println("I don't know what I am really passionate about. My favorite color is red");
+    }
+    
+}
