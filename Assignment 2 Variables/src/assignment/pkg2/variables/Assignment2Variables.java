74fae17f22bb1c079a7ae51d653027b344dd4d20
Jesus Resendiz <19jresendizestrella@nuview.k12.ca.us>
2017-07-10 10:40:22 -0700

    Variables

---
+++ Assignment 2 Variables/src/assignment/pkg2/variables/Assignment2Variables.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment.pkg2.variables;
+
+/**
+ *
+ * @author Education Unlimited
+ */
+public class Assignment2Variables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int age=15;
+        double height=69.5;
+        String firstName = "Jesus";
+        String lastName = "Resendiz";
+        boolean AStudent = true;
+        String cartoon = "spongebob";
+        String birthday = "November 4th, 2001";
+        System.out.println("My name is " + firstName + " " + lastName);
+        System.out.println("I am " + age + " " + height);
+        System.out.println("My bithday is " + birthday + " and my favorite cartoon is " + cartoon);
+    }
+    
+}
